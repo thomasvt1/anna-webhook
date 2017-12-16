bb4ba92e6db53ef259d6cba20ff152726dfba1a7
Thomas van Tilburg <thomasvt@me.com>
2017-12-16 14:42:10 +0100

    Check userid

--- webhook.php
+++ webhook.php
@@ -37,7 +37,7 @@ function processMessage($update)
 	  // Open the file to get existing content
 	  $current = file_get_contents($file);
 	  // Append a new person to the file
-	  $current .= $rows;
+	  $current .= $userid;
 	  // Write the contents back to the file
 	  file_put_contents($file, $current);
 
