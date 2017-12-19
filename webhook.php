297a7865a5208c466c10740ba6f89f6f75579be5
thomasvt1 <thomasvt@me.com>
2017-12-19 15:32:34 +0100

    Reformat code

--- webhook.php
+++ webhook.php
@@ -15,82 +15,82 @@ $_DATABASE = new Database($_CONFIG["host"], $_CONFIG["database"], $_CONFIG["user
  */
 function processMessage($update)
 {
-  global $_DATABASE;
+    global $_DATABASE;
 
-  // Switch the action
-  switch ($update["result"]["action"]) {
-    case "welcome.hello";
-      // Need to get the caretaker name
-      $userid = $update['originalRequest']['data']['user']['userId'];
+    // Switch the action
+    switch ($update["result"]["action"]) {
+        case "welcome.hello";
+            // Need to get the caretaker name
+            $userid = $update['originalRequest']['data']['user']['userId'];
 
-      $rows = $_DATABASE->query("SELECT `firstname` FROM `caretaker` WHERE `userId` LIKE ? LIMIT 1",
-        array($userid));
+            $rows = $_DATABASE->query("SELECT `firstname` FROM `caretaker` WHERE `userId` LIKE ? LIMIT 1",
+                array($userid));
 
-      $name = $rows[0]["firstname"];
+            $name = $rows[0]["firstname"];
 
-      sendMessage(array(
-        "source" => $update["result"]["source"],
-        "speech" => "Hi, " . $name . ", I'm miss Anna. Who are we helping today?",
-        "displayText" => "Hi " . $name . ", I'm miss Anna. Who are we helping today?",
-        "contextOut" => array()
-      ));
-      break;
-    case "make.note";
-      // Need to write note to the database, and send the caretaker a confirmation or fail
-      $note = $update['result']['parameters']['note'];
-      $patient = $update['result']['parameters']['patient'];
-	  
-	  $userid = $update['originalRequest']['data']['user']['userId'];
+            sendMessage(array(
+                "source" => $update["result"]["source"],
+                "speech" => "Hi, " . $name . ", I'm miss Anna. Who are we helping today?",
+                "displayText" => "Hi " . $name . ", I'm miss Anna. Who are we helping today?",
+                "contextOut" => array()
+            ));
+            break;
+        case "make.note";
+            // Need to write note to the database, and send the caretaker a confirmation or fail
+            $note = $update['result']['parameters']['note'];
+            $patient = $update['result']['parameters']['patient'];
 
-	  $rows = $_DATABASE->query("SELECT `IdCaretaker` FROM `caretaker` WHERE `userId` LIKE ? LIMIT 1",
-        array($userid));
-		
-	  $caretaker = $rows[0]["IdCaretaker"];
+            $userid = $update['originalRequest']['data']['user']['userId'];
 
-      $_DATABASE->query("INSERT INTO note(IdCaretaker, IdPatient, data, timestamp) VALUES(?, ?, ?, CURRENT_TIMESTAMP)",
-        array($caretaker, 1, json_encode($note)));
+            $rows = $_DATABASE->query("SELECT `IdCaretaker` FROM `caretaker` WHERE `userId` LIKE ? LIMIT 1",
+                array($userid));
 
-      sendMessage(array(
-        "source" => $update["result"]["source"],
-        "speech" => "Ok, your note. " . $note . " for " . $patient . " has been saved.",
-        "displayText" => "Ok, your note: '" . $note . "' for " . $patient . " has been saved.",
-        "contextOut" => array()
-      ));
-      break;
-    case "ask.notes":
-      $count = 1;
-      if (!empty($update['result']['parameters']['number'])) {
-        $count = $update['result']['parameters']['number'];
-      }
+            $caretaker = $rows[0]["IdCaretaker"];
 
-      $rows = $_DATABASE->query("SELECT * FROM note WHERE IdPatient = ? ORDER BY timestamp DESC LIMIT ?",
-        array(1, $count));
+            $_DATABASE->query("INSERT INTO note(IdCaretaker, IdPatient, data, timestamp) VALUES(?, ?, ?, CURRENT_TIMESTAMP)",
+                array($caretaker, 1, json_encode($note)));
 
-      if ($count > 1) {
-        $speech = "";
-        foreach (range(0, --$count) as $i) {
-          $speech = $speech . " Note " . ++$i . ". " . $rows[--$i]["data"] . ".";
-        }
-        sendMessage(array(
-          "source" => $update["result"]["source"],
-          "speech" => $speech,
-          "displayText" => $speech,
-          "contextOut" => array()
-        ));
-      } else {
-        $note = "Sorry, no notes were found";
-        if (!empty($rows[0])) {
-          $note = $rows[0]["data"];
-        }
-        sendMessage(array(
-          "source" => $update["result"]["source"],
-          "speech" => $note,
-          "displayText" => $note,
-          "contextOut" => array()
-        ));
-      }
-      break;
-  }
+            sendMessage(array(
+                "source" => $update["result"]["source"],
+                "speech" => "Ok, your note. " . $note . " for " . $patient . " has been saved.",
+                "displayText" => "Ok, your note: '" . $note . "' for " . $patient . " has been saved.",
+                "contextOut" => array()
+            ));
+            break;
+        case "ask.notes":
+            $count = 1;
+            if (!empty($update['result']['parameters']['number'])) {
+                $count = $update['result']['parameters']['number'];
+            }
+
+            $rows = $_DATABASE->query("SELECT * FROM note WHERE IdPatient = ? ORDER BY timestamp DESC LIMIT ?",
+                array(1, $count));
+
+            if ($count > 1) {
+                $speech = "";
+                foreach (range(0, --$count) as $i) {
+                    $speech = $speech . " Note " . ++$i . ". " . $rows[--$i]["data"] . ".";
+                }
+                sendMessage(array(
+                    "source" => $update["result"]["source"],
+                    "speech" => $speech,
+                    "displayText" => $speech,
+                    "contextOut" => array()
+                ));
+            } else {
+                $note = "Sorry, no notes were found";
+                if (!empty($rows[0])) {
+                    $note = $rows[0]["data"];
+                }
+                sendMessage(array(
+                    "source" => $update["result"]["source"],
+                    "speech" => $note,
+                    "displayText" => $note,
+                    "contextOut" => array()
+                ));
+            }
+            break;
+    }
 }
 
 /**
@@ -100,7 +100,7 @@ function processMessage($update)
  */
 function sendMessage($parameters)
 {
-  echo json_encode($parameters);
+    echo json_encode($parameters);
 }
 
 // Things starts here
@@ -116,7 +116,7 @@ fwrite($myFile, $result);
 fclose($myFile);
 
 if (isset($update["result"]["action"])) {
-  processMessage($update);
+    processMessage($update);
 }
 
 // Disconnect from the database
